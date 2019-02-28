d63305e0ee98b66267b19807563b710b221ac150
CameronBYUI <cameronrjohnson93@gmail.com>
2019-02-28 14:20:39 -0600

    Sign in existing users created but not implemented Sign out created but not implemented
    Signed-off-by: CameronBYUI <cameronrjohnson93@gmail.com>

--- app/src/main/java/byuics246/budgeting/MainActivity.java
+++ app/src/main/java/byuics246/budgeting/MainActivity.java
@@ -89,6 +89,40 @@ public class MainActivity extends AppCompatActivity {
         // [END create_user_with_email]
     }
 
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
     public void openRegisterPage() {
         Intent registerIntent = new Intent(this, RegisterActivity.class);
         startActivity(registerIntent);
