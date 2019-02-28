959f5e11cace17eb2d52e63285d772fd120269d1
CameronBYUI <cameronrjohnson93@gmail.com>
2019-02-28 14:08:12 -0600

    Email and password validation Sign Up new users created but not implemented
    Signed-off-by: CameronBYUI <cameronrjohnson93@gmail.com>

--- app/src/main/java/byuics246/budgeting/MainActivity.java
+++ app/src/main/java/byuics246/budgeting/MainActivity.java
@@ -2,21 +2,39 @@ package byuics246.budgeting;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "Main";
+    private EditText mEmailField;
+    private EditText mPasswordField;
+
+    // [Start declare_auth]
     private FirebaseAuth mAuth;
+    // [End declare_auth]
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Views
+        mEmailField = findViewById(R.id.editTextSignInUsername);
+        mPasswordField = findViewById(R.id.editTextSignInPassword);
 
         // Buttons
         final Button register = findViewById(R.id.buttonSignInRegister);
@@ -31,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
     }
 
+    // [START on_start_check_user]
     @Override
     public void onStart() {
         super.onStart();
@@ -38,7 +57,37 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
     }
+    // [END on_start_check_user]
 
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+        // [END create_user_with_email]
+    }
 
     public void openRegisterPage() {
         Intent registerIntent = new Intent(this, RegisterActivity.class);
@@ -48,4 +97,27 @@ public class MainActivity extends AppCompatActivity {
     private void updateUI(FirebaseUser user) {
 
     }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
 }
