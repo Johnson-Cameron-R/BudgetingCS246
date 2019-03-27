ccb890fb221c927748ea5c2a4d2231a6128eb16c
Cameron <cameronrjohnson93@gmail.com>
2019-03-26 22:15:19 -0500

    Income view complete

--- app/src/main/java/byuics246/budgeting/IncomeActivity.java
+++ app/src/main/java/byuics246/budgeting/IncomeActivity.java
@@ -2,12 +2,27 @@ package byuics246.budgeting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class IncomeActivity extends AppCompatActivity {
+public class IncomeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private static final String TAG = "Income";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
